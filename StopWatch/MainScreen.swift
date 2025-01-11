87237c30b9def6809e35b1ff1f4cfe13dd8dbf13
aydinnkaya <Aydinnkaya1@gmail.com>
2025-01-11 13:04:57 +0300

    Initial Commit

---
+++ StopWatch/MainScreen.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by Aydın KAYA on 11.01.2025.
+//
+
+import UIKit
+
+class MainScreen: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var resetButtonLabel: UIButton!
+    @IBOutlet weak var startStopButtonLabel: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func startStopButton(_ sender: UIButton, forEvent event: UIEvent) {
+        
+    }
+    
+   
+    
+    @IBAction func resetButton(_ sender: UIButton, forEvent event: UIEvent) {
+    }
+    
+    
+    
+    
+
+
+}
+
---
+++ StopWatchTests/StopWatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchTests.swift
+//  StopWatchTests
+//
+//  Created by Aydın KAYA on 11.01.2025.
+//
+
+import Testing
+@testable import StopWatch
+
+struct StopWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
